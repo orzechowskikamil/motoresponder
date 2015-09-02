ae89f92e85d876193a218c53291886a8295b658d
OrzechowskiKamil <orzechowski.kamil@pollub.edu.pl>
2015-09-03 00:37:08 +0200

    beautified sms Receiver

--- app/src/main/java/com/medziku/myapplication/MainActivity.java
+++ app/src/main/java/com/medziku/myapplication/MainActivity.java
@@ -412,9 +412,9 @@ public class MainActivity extends Activity {
         public void onReceive(Context context, Intent intent) {
             // TODO Auto-generated method stub
 
-            boolean smsReceived = intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED");
+            boolean receivedIntentIsSMS = intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED");
 
-            if (smsReceived) {
+            if (receivedIntentIsSMS) {
                 Log.v(TAG, "SMS received");
                 Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
 
@@ -423,8 +423,6 @@ public class MainActivity extends Activity {
                 //TODO process message for incoming number, startActivity MainActivity to execute onStartCommand to run send back gps info sms
 
                     SmsMessage[] msgs = null;
-                    String msg_from;
-
 
                     //---retrieve the SMS message received---
                     try {
@@ -432,7 +430,8 @@ public class MainActivity extends Activity {
                         msgs = new SmsMessage[pdus.length];
                         for (int i = 0; i < msgs.length; i++) {
                             msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                            msg_from = msgs[i].getOriginatingAddress();
+
+                            String msg_from = msgs[i].getOriginatingAddress();
                             String msgBody = msgs[i].getMessageBody();
                         }
                     } catch (Exception e) {
