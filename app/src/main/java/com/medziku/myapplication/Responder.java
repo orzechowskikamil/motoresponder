c3288a24fe9f14aef79d4478c71479079d54365b
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2015-09-08 10:19:34 +0200

    Update Responder.java

--- app/src/main/java/com/medziku/myapplication/Responder.java
+++ app/src/main/java/com/medziku/myapplication/Responder.java
@@ -9,7 +9,7 @@ public class Responder {
     private int waitBeforeHandling = 10000;
 
     public Responder() {
-
+        // probably we have to start every onsmsreceived in new thread
     }
 
     public void onSMSReceived(String phoneNumber) {
@@ -20,27 +20,12 @@ public class Responder {
     public void onCallReceived(String phoneNumber) {
         this.handleIncoming(phoneNumber);
     }
-
     public void cancelAllHandling() {
 
     }
+    
 
-    private void handleIncoming(String phoneNumber) {
-        if (this.phoneIsLocked() == false) {
-            return;
-        }
-
-        this.wait(this.waitBeforeHandling);
-
-        if (this.phoneIsLocked() == false) {
-            return;
-        }
-
-        if (this.phoneIsMoving() == false) {
-            return;
-        }
-
-    }
+   
 
     private boolean phoneIsMoving() {
         return false;
