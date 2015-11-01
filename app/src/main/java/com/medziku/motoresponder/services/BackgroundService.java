a259d84a50f01ec96bc30fda635c64bd323abe58
OrzechowskiKamil <orzechowski.kamil@pollub.edu.pl>
2015-11-01 13:06:22 +0100

    backgroundservice stripped from logic! renamed RespondingDecider to RespondingDecision
    Compatible with SHA 1ea49b60

--- app/src/main/java/com/medziku/motoresponder/services/BackgroundService.java
+++ app/src/main/java/com/medziku/motoresponder/services/BackgroundService.java
@@ -1,35 +1,22 @@
 package com.medziku.motoresponder.services;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.app.TaskStackBuilder;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.medziku.motoresponder.R;
+
 import com.medziku.motoresponder.Responder;
-import com.medziku.motoresponder.activity.SettingsActivity;
-import com.medziku.motoresponder.callbacks.CallCallback;
-import com.medziku.motoresponder.callbacks.SMSReceivedCallback;
-import com.medziku.motoresponder.utils.*;
+
 
 /**
- * Created by medziku on 22.09.15.
+ * Works in background and serves responses to unanswered calls and messages.
  */
 public class BackgroundService extends Service {
 
-    private int notificationId = 0;
-
-    //from activity
 
     private Responder responder;
-
-    //end from activity
 
 
     @Nullable
@@ -45,48 +32,20 @@ public class BackgroundService extends Service {
 
     @Override
     public void onDestroy() {
+        this.responder.stopResponding();
         super.onDestroy();
-        hideNotification();
-//        this.sensorsUtility.unregisterSensors();//TODO powinna byc takze mozliwosc wyrejestrowania w on command started dla konkretnej komendy
+        //TODO powinna byc takze mozliwosc wyrejestrowania w on command started dla konkretnej komendy
+//        this.sensorsUtility.unregisterSensors();
         Log.d("BackgroundService", "destroed");
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-
         Log.d("BackgroundService", "created");
 
-        //from activity
-
-
-        SMSUtility smsUtility = new SMSUtility(this);
-        CallsUtility callsUtility = new CallsUtility(this);
-
-
         this.responder = new Responder(this);
-
-        smsUtility.listenForSMS(new SMSReceivedCallback() {
-            @Override
-            public void onSMSReceived(String phoneNumber, String message) {
-                BackgroundService.this.onSMSReceived(phoneNumber, message);
-            }
-        });
-
-        callsUtility.listenForCalls(new CallCallback() {
-            @Override
-            public void onCall(String phoneNumber) {
-                BackgroundService.this.onCallReceived(phoneNumber);
-            }
-        });
-
-        //end from activity
-
-        showNotification();
-
-        // TODO K. Orzechowski: remove it later because its only for development
-
-        this.responder.onSMSReceived("79146755");
+        this.responder.startResponding();
     }
 
 
@@ -99,65 +58,4 @@ public class BackgroundService extends Service {
     public void onRebind(Intent intent) {
         super.onRebind(intent);
     }
-
-
-    //mine
-
-    public void showStupidNotify(String title, String content) {
-        this.hideNotification();
-        this.showNotification(title, content, "test info");
-    }
-
-
-    private void showNotification() {
-        this.showNotification("Test title", "Test content", "test info");
-    }
-
-    private void showNotification(String title, String content, String info) {
-        Intent resultIntent = new Intent(this, SettingsActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(SettingsActivity.class);
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        Notification.Builder notificationBuilder = new Notification.Builder(getApplicationContext());
-        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(title)
-                .setContentText(content)
-                .setContentInfo(info)
-                .setContentIntent(resultPendingIntent);
-        Notification notification = notificationBuilder.build();
-        notification.flags = Notification.FLAG_ONGOING_EVENT;
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(notificationId, notification);
-    }
-
-    private void hideNotification() {
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.cancel(notificationId);
-    }
-
-
-    //from activity
-
-    private void onCallReceived(String phoneNumber) {
-        this.responder.onUnAnsweredCallReceived(phoneNumber);
-    }
-
-    private void onSMSReceived(String phoneNumber, String message) {
-        //this.showToast("SMS arrived! Phone number: " + phoneNumber + ", message: " + message);
-        // TODO change to notification
-
-
-        this.responder.onSMSReceived(phoneNumber);
-    }
-
-    //end from activity
-
-
 }
--- app/src/main/java/com/medziku/motoresponder/utils/CallsUtility.java
+++ app/src/main/java/com/medziku/motoresponder/utils/CallsUtility.java
@@ -2,60 +2,37 @@ package com.medziku.motoresponder.utils;
 
 import android.content.Context;
 import android.telephony.*;
+import com.google.common.base.Predicate;
 
-import com.medziku.motoresponder.callbacks.CallCallback;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Kamil on 2015-09-08.
- */
-
-@Deprecated
 public class CallsUtility {
 
     private Context context;
-    private List<CallCallback> callCallbacksList;
+    private Predicate<String> callCallback;
 
     public CallsUtility(Context context) {
-        // TODO: 2015-09-08 add more callbacks!
-        //medziku: no more callbacks!
         this.context = context;
-        this.callCallbacksList = new ArrayList<>();
     }
 
-    public void listenForCalls(CallCallback callCallback) {
+    public void listenForCalls(Predicate<String> callCallback) {
         TelephonyManager telephonyManager = (TelephonyManager) this.context.getSystemService(Context.TELEPHONY_SERVICE);
-        MyCallListener phoneStateListener = new MyCallListener();
-        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
-        this.callCallbacksList.add(callCallback);
-
-    }
-
-    private class MyCallListener extends PhoneStateListener {//Responsible for incoming phone calls, phone state etc
-        public String TAG = MyCallListener.class.getName();
-
-        @Override
-        public void onCallStateChanged(int state, String incomingNumber) {//Call state
-            super.onCallStateChanged(state, incomingNumber);
-            switch (state) {
-                case TelephonyManager.CALL_STATE_IDLE:
-                    break;
-                case TelephonyManager.CALL_STATE_RINGING:
-                    for (CallCallback callCallback : CallsUtility.this.callCallbacksList) {
-                        callCallback.onCall(incomingNumber);
-                    }
-                    break;
-                case TelephonyManager.CALL_STATE_OFFHOOK:
-                    break;
-                default:
-                    break;
+        PhoneStateListener phoneStateListener = new PhoneStateListener() {
+            @Override
+            public void onCallStateChanged(int state, String incomingNumber) {
+                super.onCallStateChanged(state, incomingNumber);
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        break;
+                    case TelephonyManager.CALL_STATE_RINGING:
+                        CallsUtility.this.callCallback.apply(incomingNumber);
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK:
+                        break;
+                    default:
+                        break;
+                }
             }
-        }
-    }
-
-    public List<CallCallback> getCallCallbacksList() {
-        return callCallbacksList;
+        };
+        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+        this.callCallback = callCallback;
     }
 }
