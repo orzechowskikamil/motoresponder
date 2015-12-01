9f5dbd8016488be66e44c19fdd8c78869e5fbf69
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2015-12-01 14:31:45 +0100

    SettingsUtility created

---
+++ app/src/main/java/com/medziku/motoresponder/utils/SettingsUtility.java
@@ -0,0 +1,45 @@
+class SettingsUtility{
+
+    private SharedPreferences.Editor editor;
+    public SettingsUtility(){
+      SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+      SharedPreferences.Editor this.editor = sharedPref.edit();
+      editor.putInt(getString(R.string.saved_high_score), newHighScore);
+      editor.commit();
+    }
+    
+    private boolean getValue(String name, boolean defaultValue){
+        return this.getBoolean(name,defaultValue);
+    }
+    
+    private void setValue(String name, boolean value){
+      this.editor.putBoolean(name, value);
+    }
+    
+        private String getValue(String name, String defaultValue){
+        return this.getString(name,defaultValue);
+    }
+    
+    private void setValue(String name, String value){
+      this.editor.putString(name, value);
+    }
+    
+        private int getValue(String name, int defaultValue){
+        return this.getInt(name,defaultValue);
+    }
+    
+    private void setValue(String name, int value){
+      this.editor.putInt(name, value);
+    }
+    
+    private String SERVICE_ENABLED='service-enabled';
+    
+    public boolean isServiceEnabled(){
+        return this.getValue(SERVICE_ENABLED, true);
+    }
+    
+    public void setServiceEnabled(boolean value){
+      this.setValue(SERVICE_ENABLED, value);
+    }
+    
+}
