25c6f6764caf9def8db7d0b3c861ac0ca449b7a1
OrzechowskiKamil <orzechowski.kamil@pollub.edu.pl>
2015-12-02 23:01:34 +0100

    corrected settings utility errors

--- app/src/main/java/com/medziku/motoresponder/utils/SettingsUtility.java
+++ app/src/main/java/com/medziku/motoresponder/utils/SettingsUtility.java
@@ -1,53 +1,58 @@
-class SettingsUtility{
+package com.medziku.motoresponder.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+class SettingsUtility {
+
+    private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
-    
-    public SettingsUtility(Context context){
-      SharedPreferences sharedPref = context.getActivity().getPreferences(Context.MODE_PRIVATE);
-      SharedPreferences.Editor this.editor = sharedPref.edit();
-      editor.putInt(getString(R.string.saved_high_score), newHighScore);
-      editor.commit();
+    private String SERVICE_ENABLED = "service-enabled";
+
+    public SettingsUtility(Activity activity) {
+        this.sharedPreferences = activity.getPreferences(Context.MODE_PRIVATE);
+        this.editor = this.sharedPreferences.edit();
     }
-    
-    private boolean getValue(String name, boolean defaultValue){
-        return this.getBoolean(name,defaultValue);
+
+    private boolean getValue(String name, boolean defaultValue) {
+        return this.sharedPreferences.getBoolean(name, defaultValue);
     }
-    
-    private void setValue(String name, boolean value){
-      this.editor.putBoolean(name, value);
-      this.editor.commit();
+
+    private void setValue(String name, boolean value) {
+        this.editor.putBoolean(name, value);
+        this.editor.commit();
     }
-    
-        private String getValue(String name, String defaultValue){
-        return this.getString(name,defaultValue);
+
+    private String getValue(String name, String defaultValue) {
+        return this.sharedPreferences.getString(name, defaultValue);
     }
-    
-    private void setValue(String name, String value){
-      this.editor.putString(name, value);
-      this.editor.commit();
+
+    private void setValue(String name, String value) {
+        this.editor.putString(name, value);
+        this.editor.commit();
     }
-    
-        private int getValue(String name, int defaultValue){
-        return this.getInt(name,defaultValue);
+
+    private int getValue(String name, int defaultValue) {
+        return this.sharedPreferences.getInt(name, defaultValue);
     }
-    
-    private void setValue(String name, int value){
-      this.editor.putInt(name, value);
-      this.editor.commit();
+
+    private void setValue(String name, int value) {
+        this.editor.putInt(name, value);
+        this.editor.commit();
     }
-    
-    private String SERVICE_ENABLED="service-enabled";
-    
-    public boolean isServiceEnabled(){
+
+
+    public boolean isServiceEnabled() {
         return this.getValue(SERVICE_ENABLED, true);
     }
-    
-    public void setServiceEnabled(boolean value){
-      this.setValue(SERVICE_ENABLED, value);
+
+    public void setServiceEnabled(boolean value) {
+        this.setValue(SERVICE_ENABLED, value);
     }
-    
-    public String getAutoResponseTextForSMS()(){
+
+    public String getAutoResponseTextForSMS() {
         return "(Automatyczna odpowiedz) Czesc, jezdze wlasnie motocyklem, odezwe sie jak skonczy mi sie paliwo.";
     }
-    
+
 }
