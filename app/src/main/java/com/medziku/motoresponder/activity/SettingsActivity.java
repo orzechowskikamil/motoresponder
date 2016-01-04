a04bde5da3a255e88cf544260558b08d6afb204c
medziku <medziku@med.medlap>
2016-01-04 17:24:29 +0100

    removed SwitchPreferencesFragment

--- app/src/main/java/com/medziku/motoresponder/activity/SettingsActivity.java
+++ app/src/main/java/com/medziku/motoresponder/activity/SettingsActivity.java
@@ -1,17 +1,10 @@
 package com.medziku.motoresponder.activity;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.widget.ListAdapter;
 
 import com.medziku.motoresponder.R;
-import com.medziku.motoresponder.services.BackgroundService;
-import com.medziku.motoresponder.ui.PrefsHeaderAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -20,51 +13,12 @@ import java.util.List;
  */
 public class SettingsActivity extends PreferenceActivity {
 
-    private List<Header> mHeaders;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, BackgroundService.class);
-        startService(intent);
-    }
-
-    protected void onResume() {
-        super.onResume();
-
-        if (getListAdapter() instanceof PrefsHeaderAdapter)
-            ((PrefsHeaderAdapter) getListAdapter()).resume();
-    }
-
-    protected void onPause() {
-        super.onPause();
-        if (getListAdapter() instanceof PrefsHeaderAdapter)
-            ((PrefsHeaderAdapter) getListAdapter()).pause();
-    }
-
-    @Override
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.preference_headers, target);
-
-        mHeaders = target;
-    }
-
-    public void setListAdapter(ListAdapter adapter) {
-        int i, count;
-
-        if (mHeaders == null) {
-            mHeaders = new ArrayList<Header>();
-            count = adapter.getCount();
-            for (i = 0; i < count; ++i)
-                mHeaders.add((Header) adapter.getItem(i));
-        }
-
-        super.setListAdapter(new PrefsHeaderAdapter(this, mHeaders));
-    }
-
-    @Override
-    protected boolean isValidFragment(String fragmentName) {
-        return true;
+        addPreferencesFromResource(R.xml.background_prefs);
+        /*Intent intent = new Intent(this, BackgroundService.class);
+        startService(intent);*/
     }
 }
