5a0bf17906a447d8a43ee62b2090ccb8c58917d2
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2016-04-20 12:40:42 +0200

    remove callbacks #125

--- app/src/main/java/com/medziku/motoresponder/callbacks/SMSReceivedCallback.java
+++
@@ -1,8 +0,0 @@
-package com.medziku.motoresponder.callbacks;
-
-/**
- * Created by medziku on 22.09.15.
- */
-public interface SMSReceivedCallback {
-    void onSMSReceived(String phoneNumber, String message);
-}
