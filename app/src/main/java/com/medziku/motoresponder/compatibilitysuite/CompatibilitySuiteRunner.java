d9b94d855afc0661871e09bc8668e2554c171460
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2016-07-27 12:30:27 +0200

    Update CompatibilitySuiteRunner.java

--- app/src/main/java/com/medziku/motoresponder/compatibilitysuite/CompatibilitySuiteRunner.java
+++ app/src/main/java/com/medziku/motoresponder/compatibilitysuite/CompatibilitySuiteRunner.java
@@ -1,8 +1,30 @@
+/**
+ * Purpose of this is to have suite of simple, quick tests, which we can run on any device, and which
+ * can be embedded into a feature of production app, to measure if device is compatible with an application.
+ * Those tests require real android device, but doesn't require user interaction, they look if we are able to 
+ * read required device APIs
+ */
+
+
 public class CompatibilitySuiteRunner {
     
-    
-
-
-    public void run(){
+    List<CompatibilityTestCase> testCases;
+    private Context context;
+    public CompatibilitySuiteRunner(Context context){this.context=context;this.testCases = this.createTestCases();}
+    private List<> createTestCases(){
+        list= new ArrayList<>();
+        
+        list.add(new SMSUtilityCompatibilityTestCase());
+    }
+    public boolean isDeviceCompatible(){
+        
+        for (CompatibilityTestCase case : this.testCases){
+            result = case.run();
+            
+            if (!result.isCompatible){
+                return false;
+            }
+        }
+        
     }
 }
