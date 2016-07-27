57a11621da1fd3e594ec7f542ea69df19d89a945
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2016-07-27 10:39:56 +0200

    Create CompatibilityTestCase.java

---
+++ app/src/main/java/com/medziku/motoresponder/compatibilitysuite/CompatibilityTestCase.java
@@ -0,0 +1,17 @@
+
+/**
+ * It's simple case for compatibility test. 
+ * Compatibility tests are intended to run in main app, without testing environment, 
+ * so using any test suite like JUnit is not possible.
+ */
+abstract class CompatibilityTestCase{
+
+  public CompatibilityTestResult runTest(){
+  }
+
+}
+
+public CompatibilityTestResult{
+    public boolean isCompatible;
+}
+
