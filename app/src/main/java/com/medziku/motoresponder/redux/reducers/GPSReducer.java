f43e62cd341f2136e045ba92f2e60eed61c4287d
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2017-09-07 14:30:52 +0200

    Create GPSReducer.java

---
+++ app/src/main/java/com/medziku/motoresponder/redux/reducers/GPSReducer.java
@@ -0,0 +1,17 @@
+public class GPSReducer extends Reducer<GPSState>{
+   public GPSState reduce(GPSState state,Action action){
+   Class actionType =action.getClass();
+  
+   if (actionType==GPSStartAction){
+      newState
+   }
+   
+   if (actionType==GPSStopAction){
+   }
+   
+   if (actionType==UpdateFromGPSAction){
+   
+   }
+   
+   }
+}
