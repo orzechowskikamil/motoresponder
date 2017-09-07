bd2962d7a5479e8c65f541b1fdc17baa42e38fb5
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2017-09-07 14:04:02 +0200

    Create UpdateFromGPSAction.java

---
+++ app/src/main/java/com/medziku/motoresponder/redux/actions/UpdateFromGPSAction.java
@@ -0,0 +1,9 @@
+public class UpdateFromGPSAction extends Action{
+public final double lat;
+public final double long;
+  public UpdateFromGPSAction(double lat,double long){
+    super();
+    this.lat=lat;
+    this.long=long;
+  }
+}
