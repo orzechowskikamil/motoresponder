1b6700647ff48f25b8494243963c689ed19accfa
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2017-09-14 12:58:12 +0200

    Create Actions.java

---
+++ app/src/main/java/com/medziku/motoresponder/redux/Actions.java
@@ -0,0 +1,7 @@
+public final class Actions {
+    public enum GPS {ON, OFF, LOCATION_UPDATE}
+    public enum Accelerometer {ON, OFF, MOTION_EVENT}
+    public enum RespondingProcess  { INCOMING_CALL, INCOMING_MESSAGE, RESPOND_WITH_MESSAGE   }
+    public enum Settings {  LOAD_SETTINGS, SETTINGS_UPDATED  }
+    public enum Contacts  { LOAD_CONTACTS   }
+}
