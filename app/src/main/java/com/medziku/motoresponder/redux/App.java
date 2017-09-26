9ac4e11a77b4d1a9f44cddcd93189466232a26de
orzechowskikamil <orzechowski.kamil@pollub.edu.pl>
2017-09-26 16:07:22 +0200

    Update App.java

--- app/src/main/java/com/medziku/motoresponder/redux/App.java
+++ app/src/main/java/com/medziku/motoresponder/redux/App.java
@@ -3,33 +3,26 @@ package com.medziku.motoresponder.redux;
 import trikita.jedux.Action;
 import trikita.jedux.Store;
 
-/**
- * Created by Kamil on 25.09.2017.
- */
+public class AppStore extends {
+        
+        public AppStore(Store<Action, State> store){
+                this.store=store;
+        }
+        
+        
+        public static AppStore createDefault(){
+                return new AppStore(new Store<Action,State>(new Reducer(),                   Default.build()));
+                
+        }
+       
 
-public class App {
-
-        private static App instance;
-
-        private Store<Action, State> store;
-
-        public static State dispatch(Action action) {
-            return instance.store.dispatch(action);
+        public  State dispatch(Action action) {
+            return this.store.dispatch(action);
         }
 
-        public static State getState() {
-            return instance.store.getState();
+        public  State getState() {
+            return this.store.getState();
         }
 
 
-        public void onCreate() {
-            App.instance = this;
-
-
-
-            this.store = new Store<Action,State>(new Reducer(),
-                    Default.build());
-
-            this.store.subscribe(Anvil::render);
-        }                                                                                                 :
     }
